5e226174b6e933567a72e0286aa27aa8d8800396
赵小强 <944974339@qq.com>
2022-07-19 00:13:31 +0800

    配置项

--- src/app.js
+++ src/app.js
@@ -5,8 +5,12 @@ const json = require("koa-json");
 const onerror = require("koa-onerror");
 const bodyparser = require("koa-bodyparser");
 const logger = require("koa-logger");
+const session = require("koa-generic-session");
+const redisStore = require("koa-redis");
 
+const { REDIS_CONF } = require("./conf/db");
 const { isProd } = require("./utils/env");
+const { SESSION_SECRET_KEY } = require("./conf/secretKeys");
 
 // 路由
 const index = require("./routes/index");
@@ -35,6 +39,23 @@ app.use(require("koa-static")(__dirname + "/public"));
 app.use(
   views(__dirname + "/views", {
     extension: "ejs",
+  })
+);
+
+// session 配置
+app.keys = [SESSION_SECRET_KEY];
+app.use(
+  session({
+    key: "weibo.sid", // cookie name 默认是 `koa.sid`
+    prefix: "weibo:sess:", // redis key 的前缀，默认是 `koa:sess:`
+    cookie: {
+      path: "/",
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000, // 单位 ms
+    },
+    store: redisStore({
+      all: `${REDIS_CONF.host}:${REDIS_CONF.port}`,
+    }),
   })
 );
 
