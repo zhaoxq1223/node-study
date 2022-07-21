e990ea56922de89a5c429cd04436b4af0002bf73
赵小强 <944974339@qq.com>
2022-07-21 23:00:15 +0800

    判断已登录情况页面状态

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,11 +1,32 @@
 const router = require("koa-router")();
 
+/**
+ * @description: 获取登录信息
+ * @param {Object} ctx
+ * @return {Object}
+ */
+const getLoginInfo = (ctx) => {
+  let data = {
+    isLogin: false,
+  };
+
+  const userInfo = ctx.session.userInfo;
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName,
+    };
+  }
+
+  return data;
+};
+
 router.get("/login", async (ctx, next) => {
-  await ctx.render("login", {});
+  await ctx.render("login", getLoginInfo(ctx));
 });
 
 router.get("/register", async (ctx, next) => {
-  await ctx.render("register", {});
+  await ctx.render("register", getLoginInfo(ctx));
 });
 
 module.exports = router;
