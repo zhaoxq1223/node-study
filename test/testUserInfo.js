94ead3cf950e0e285825315bb298eda252d540d3
赵小强 <944974339@qq.com>
2022-07-31 17:59:07 +0800

    test首页

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -1,11 +1,10 @@
 module.exports = {
-  L_ID: 1,
-  L_USER_NAME: "lisi",
-  L_COOKIE:
-    "weibo.sid=fzjUYPKV9cTGmaRQNY0KCfuMMMo9truK; weibo.sid.sig=GAVhaHWU01oyuuzBGoOH6tdn-Y8",
-
-  Z_ID: 2,
+  Z_ID: 1,
   Z_USER_NAME: "zhangsan",
   Z_COOKIE:
-    "weibo.sid=maiFbtzawvTbXDX3_0aOuY5FIjJ5gm-z; weibo.sid.sig=Nue7eE9Oaq0gTCzxqk19Dp4jD2I",
+    "weibo.sid=fzjUYPKV9cTGmaRQNY0KCfuMMMo9truK; weibo.sid.sig=GAVhaHWU01oyuuzBGoOH6tdn-Y8",
+  L_ID: 2,
+  L_USER_NAME: "lisi",
+  L_COOKIE:
+    "weibo.sid=eSZoTAPwIKffjaC6Pt_thYFFl6KIWf1P; weibo.sid.sig=883CuTW_QMyIwxuJhLT3t8ZTl-Q",
 };
